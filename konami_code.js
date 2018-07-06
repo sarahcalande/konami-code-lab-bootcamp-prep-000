ca24e058f7b6cdc24cd8b7a13b72930992e69f61
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 08:00:45 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,40 +1,19 @@
-const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
-];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  var index = 0;
+  document.addEventListener('keydown', function (e) {
+    const key = parseInt(e.detail || e.which);
 
-// check to make sure that the browser can handle window.addEventListener
-if (window.addEventListener) {
-    // create the keys and konami variables
-    var keys = [],
-        konami = "38,38,40,40,37,39,37,39,66,65";
-    
-    // bind the keydown event to the Konami function
-    window.addEventListener("keydown", function(e){
-        // push the keycode to the 'keys' array
-        keys.push(e.keyCode);
-        
-        // and check to see if the user has entered the Konami code
-        if (keys.toString().indexOf(konami)) {
-            // do something such as:
-            alert('Konami');
-            
-            // and finally clean up the keys array
-            keys = [];
-        }
-        
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length - 1) {
+        window.alert("YOU DID IT!");
+        index = 0;
+      }
+    } else {
+      index = 0;
     }
-    );
-     true;
-}
+  }, false);
 }
