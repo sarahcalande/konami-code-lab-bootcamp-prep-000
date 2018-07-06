fbfedcb410016b618575a208b7afd4fc1ec9487b
scalande@yahoo.com <scalande@yahoo.com>
2018-07-06 08:00:39 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,29 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+
+// check to make sure that the browser can handle window.addEventListener
+if (window.addEventListener) {
+    // create the keys and konami variables
+    var keys = [],
+        konami = "38,38,40,40,37,39,37,39,66,65";
+    
+    // bind the keydown event to the Konami function
+    window.addEventListener("keydown", function(e){
+        // push the keycode to the 'keys' array
+        keys.push(e.keyCode);
+        
+        // and check to see if the user has entered the Konami code
+        if (keys.toString().indexOf(konami)) {
+            // do something such as:
+            alert('Konami');
+            
+            // and finally clean up the keys array
+            keys = [];
+        }
+        
+    }
+    );
+     true;
+}
 }
